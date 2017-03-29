76bfa1c1b446b59c31a4c67c2738151a303fe864
xxyzz <elephantpolar@gmail.com>
2017-03-29 10:41:46 +0800

    Add share function

--- js/quote.js
+++ js/quote.js
@@ -1,5 +1,6 @@
 var colors = ['#f44336', ' #e91e63', ' #9c27b0', '#673ab7', '#3f51b5', ' #2196f3', ' #009688', ' #cddc39', '#ffc107', ' #ff5722', ' #795548', ' #607d8b'];
 var authors = ['Augustus', 'Lucius_Cornelius_Sulla', 'Seneca_the_Younger', 'Pliny_the_Elder', 'Marcus_Aurelius'];
+var currentQuote = "";
 
 var WikiquoteApi = (function() {
 
@@ -253,7 +254,7 @@ function changeColor() {
     $("body, #new-quote").css({
         backgroundColor: colors[color]
     });
-    $("#wiki, .mdl-card__title-text, .mdl-card__supporting-text, .mdl-card__menu").css({
+    $("#wiki, .mdl-card__title-text, .mdl-card__supporting-text, #share").css({
         color: colors[color]
     });
     var metaThemeColor = document.querySelector("meta[name=theme-color]");
@@ -264,13 +265,12 @@ function getQuote() {
 
     var author = Math.floor(Math.floor(Math.random() * authors.length));
     WikiquoteApi.getRandomQuote(authors[author], function(quote) {
-        console.log(quote.quote);
         var imageURL = './images/' + authors[author] + ".jpg";
         var wikiLink = 'https://en.wikiquote.org/wiki/' + authors[author];
-        console.log(imageURL);
         if (quote.quote.length >= 5) {
             $(".mdl-card__title").css('background', 'url(' + imageURL + ') center / cover');
-            $(".mdl-card__title-text").text(authors[author]);
+            $(".mdl-card__title-text").text(authors[author].split('_').join(' '));
+            currentQuote = quote.quote + ' --' + authors[author].split('_').join(' ');
             $("#text").text(quote.quote);
             $("#wiki").attr('href', wikiLink);
             changeColor();
@@ -284,4 +284,7 @@ function getQuote() {
 $(document).ready(function() {
     getQuote();
     $("#new-quote").on("click", getQuote);
+    $("#share").on("click", function() {
+        window.open("https://telegram.me/share/url?url=https://xxyzz.github.io&text=" + currentQuote);
+    });
 });
