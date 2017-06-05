f62340894006b8bea934a4ab02b0db92387715a0
xxyzz <elephantpolar@gmail.com>
2017-06-05 21:43:38 +0800

    change mode button

--- js/tic.js
+++ js/tic.js
@@ -1,59 +1,76 @@
 document.addEventListener('DOMContentLoaded', function() {
   // default set
-  var single_mode = true;
+  var mode = 1; // 0: easy 1: medium 2: impossible 3: double
   var x_turn = true;
   var user_first = true; // user choose x
   var x_point = 0;
   var o_point = 0;
-  var result = 3;
+  var result = 4; // 0: draw 1: x win 2: o win 3: game on 4: start game
   var checkerboard = [
     0, 0, 0, 0, 0, 0, 0, 0, 0
   ];
 
-  // single or double button
-  document.getElementById('mode').addEventListener('click', function() {
-    single_mode = !single_mode;
-    var data = "";
-    if (single_mode) {
+  // set mode
+  document.getElementById('easy').addEventListener('click', function() {
+    if (mode !== 0) {
+      mode = 0;
       data = {
-        message: "Play against computer"
+        message: "Easy mode, you go first or click O button let computer go first"
       };
-      document.getElementById('mode').childNodes[1].textContent = "person";
-    } else {
+      document.getElementById('toast').MaterialSnackbar.showSnackbar(data);
+      document.getElementById('drop_down').children[1].textContent = "Easy";
+      restart();
+    }
+  });
+  document.getElementById('medium').addEventListener('click', function() {
+    if (mode !== 1) {
+      mode = 1;
+      data = {
+        message: "Medium mode, you go first or click O button let computer go first"
+      };
+      document.getElementById('toast').MaterialSnackbar.showSnackbar(data);
+      document.getElementById('drop_down').children[1].textContent = "Medium";
+      restart();
+    }
+  });
+  document.getElementById('impossible').addEventListener('click', function() {
+    if (mode !== 2) {
+      mode = 2;
+      data = {
+        message: "Impossible mode, you go first or click O button let computer go first"
+      };
+      document.getElementById('toast').MaterialSnackbar.showSnackbar(data);
+      document.getElementById('drop_down').children[1].textContent = "Impossible";
+      restart();
+    }
+  });
+  document.getElementById('double').addEventListener('click', function() {
+    if (mode !== 3) {
+      mode = 3;
       data = {
         message: "Play against a friend"
       };
-      document.getElementById('mode').childNodes[1].textContent = "people";
+      document.getElementById('toast').MaterialSnackbar.showSnackbar(data);
+      document.getElementById('drop_down').children[1].textContent = "Play against a friend";
+      restart();
     }
-    document.getElementById('toast').MaterialSnackbar.showSnackbar(data);
-    restart();
   });
 
   // restart button
   document.getElementById('restart').addEventListener('click', function() {
     restart();
-    single_mode = true;
+    mode = 1;
     user_first = true;
     data = {
       message: "Restart game"
     };
     document.getElementById('toast').MaterialSnackbar.showSnackbar(data);
-    document.getElementById('mode').childNodes[1].textContent = "person";
-  });
-
-  // X player button
-  document.getElementById('o').addEventListener('click', function() {
-    if (single_mode) {
-      user_first = true;
-      x_turn = true;
-    }
   });
 
   // O player button
   document.getElementById('o').addEventListener('click', function() {
-    if (single_mode) {
+    if (mode != 3 && result === 4) {
       user_first = false;
-      x_turn = true;
       computer();
     }
   });
@@ -70,7 +87,7 @@ document.addEventListener('DOMContentLoaded', function() {
   function clear_board() {
     x_turn = true;
     user_first = true;
-    result = 3;
+    result = 4;
     checkerboard = [
       0, 0, 0, 0, 0, 0, 0, 0, 0
     ];
@@ -78,7 +95,7 @@ document.addEventListener('DOMContentLoaded', function() {
     for (var i = 0; i < pieces.length; i++) {
       pieces[i].style.visibility = "hidden";
     }
-    if (single_mode && !user_first) {
+    if (mode !== 3 && !user_first) {
       computer();
     }
   }
@@ -87,13 +104,14 @@ document.addEventListener('DOMContentLoaded', function() {
   var grids = document.getElementsByClassName('grid');
   Array.prototype.forEach.call(grids, function(grid) {
     grid.addEventListener('click', function() {
-      if (checkerboard[this.id] === 0) {
+      if (checkerboard[this.id] === 0 && (result === 3 || result == 4)) {
         if (x_turn) {
           this.children[0].children[1].style.visibility = "visible";
           checkerboard[this.id] = 1;
+          result = 3;
           show_result(check_win());
           x_turn = !x_turn;
-          if (single_mode && result === 3) {
+          if (mode !== 3 && result === 3) {
             computer();
           }
         } else {
@@ -101,7 +119,7 @@ document.addEventListener('DOMContentLoaded', function() {
           checkerboard[this.id] = 2;
           show_result(check_win());
           x_turn = !x_turn;
-          if (single_mode && result === 3) {
+          if (mode !== 3 && result === 3) {
             computer();
           }
         }
@@ -149,6 +167,9 @@ document.addEventListener('DOMContentLoaded', function() {
         result = 2;
         o_point += 1;
         break;
+      default:
+        result = 3;
+        break;
     }
 
     if (result !== 3) {
@@ -163,13 +184,14 @@ document.addEventListener('DOMContentLoaded', function() {
   function computer() {
     var moves = 0;
     var avaliable_position = get_available_positon(checkerboard);
+    var run_board = checkerboard.slice();
     if (avaliable_position.length !== 0) {
-      moves = avaliable_position[Math.floor(Math.random() * avaliable_position.length)];
+      moves = get_moves(mode, run_board, avaliable_position);
       var show = document.getElementById(moves);
-      if (user_first) {
+      if (user_first && !x_turn) {
         checkerboard[moves] = 2;
         show.children[0].children[0].style.visibility = "visible";
-      } else {
+      } else if (!user_first && x_turn) {
         checkerboard[moves] = 1;
         show.children[0].children[1].style.visibility = "visible";
       }
@@ -179,6 +201,7 @@ document.addEventListener('DOMContentLoaded', function() {
       show_result(0);
     }
   }
+
   // get avaliable positon
   function get_available_positon(board) {
     var indexes = [],
@@ -187,6 +210,41 @@ document.addEventListener('DOMContentLoaded', function() {
       indexes.push(i);
     }
     return indexes;
+  }
+
+  // get moves
+  function get_moves(mode, board, avaliable_moves) {
+    var run_move = 0, run_result = 3, depth = 0, scores = [], moves = [];
+    
+    if (mode === 0) {
+      get_random_move(avaliable_moves);
+      return run_move;
+    } else if (mode === 1) {
+      if (Math.random() < 0.5) {
+        get_random_move(avaliable_moves);
+        return run_move;
+      } else {
+        run_move = get_best_move(board, avaliable_moves);
+        return run_move;
+      }
+    } else {
+      run_move = get_best_move(board, avaliable_moves);
+      return run_move;
+    }
+
+    // get random move
+    function get_random_move(avaliable_moves) {
+      run_move = avaliable_moves[Math.floor(Math.random() * board.length)];
+    }
+
+    // get best move
+    function get_best_move(board, avaliable_moves) {
+      if (avaliable_moves.length === 0 || run_result !== 3) {
+        depths.push()
+      } else if (moves.length === ) {
+
+      }
+    }
   }
 
 });
